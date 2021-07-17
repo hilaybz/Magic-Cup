8576f4859acef05b5e7b26155c018c9aaa0d57e2
hilaybz <49829703+hilaybz@users.noreply.github.com>
2021-07-17 13:17:57 +0300

    Add files via upload

--- Cup URL Change.py
+++ Cup URL Change.py
@@ -5,9 +5,10 @@ secret_key = "61b113ac679d5d0e2ab28c82bb2c3f9c"
 short_url = "sz7aqn"
 # url = "http://google.com"
 
-with open('URL Bank.txt') as r:  # text file with URLs
-    url = r.readlines(1)  # need to clean url
-r.close()
-print(url)
+with open('URL Bank.txt', "r") as f:  # text file with URLs
+    url = f.readline()  # reads URL line
+f.close()
+
+print(url)  # check
 
 r = requests.get(f'https://free.qrd.by/api/short?secretkey={secret_key}&shorturl={short_url}&url={url}')  # change URL
